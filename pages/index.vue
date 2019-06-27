3cd06796ed2f16b73bea5507afa68b0d1cce3171
clement <clement@antidots.com>
2019-06-27 11:54:23 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .section.container
+    h1 CSS-LOADER TEST
+    p.test Test
+</template>
+
+<style lang="scss" scoped>
+.test {
+  font-size: 110%;
+  font-weight: 900;
+}
+</style>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
---
+++ plugins/buefy.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Buefy from 'buefy'
+
+Vue.use(Buefy, {
+  defaultIconComponent: 'fa',
+  materialDesignIcons: false,
+  defaultIconPack: 'fas'
+})
